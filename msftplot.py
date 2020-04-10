6dba152babe868099cd1263f9ba030311fe72143
brandoboyd <brando.boyd@gmail.com>
2020-04-09 22:32:58 -0700

    Create msftplot.py
    use matplotlib to graph the adjusted close for msft stock

---
+++ msftplot.py
@@ -0,0 +1,16 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+style.use('ggplot')
+
+#start = dt.datetime(2000,1,1)
+#end = dt.datetime(2018,12,31)
+
+#df = web.DataReader('MSFT', 'yahoo', start, end)
+#df.to_csv('msft.csv')
+df = pd.read_csv('msft.csv', parse_dates =True, index_col=0)
+
+df['Adj Close'].plot()
