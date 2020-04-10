6cb2ef7af12521e8c27d9bde632b6860014423f0
brandoboyd <brando.boyd@gmail.com>
2020-04-09 22:41:09 -0700

    Update msftplot.py
    Update script to allow writing dataframe from past year, save to csv file, read csv, make the dates machine readable and remove index columns. Select the adjusted close column to plot against the timeline.

--- msftplot.py
+++ msftplot.py
@@ -4,11 +4,13 @@ from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
-import msft
-
 style.use('ggplot')
 
+start = dt.datetime(2019,4,8)
+end = dt.datetime(2020,4,8)
 
+df = web.DataReader('MSFT', 'yahoo', start, end)
+df.to_csv('msft.csv')
 df = pd.read_csv('msft.csv', parse_dates =True, index_col=0)
 
 df['Adj Close'].plot()
