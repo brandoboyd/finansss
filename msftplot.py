8b2bf976e07273ea72656cc8ae25461560a018f6
brandoboyd <brando.boyd@gmail.com>
2020-04-09 22:36:16 -0700

    Update msftplot.py

--- msftplot.py
+++ msftplot.py
@@ -4,13 +4,11 @@ from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
+import msft
+
 style.use('ggplot')
 
-#start = dt.datetime(2000,1,1)
-#end = dt.datetime(2018,12,31)
 
-#df = web.DataReader('MSFT', 'yahoo', start, end)
-#df.to_csv('msft.csv')
 df = pd.read_csv('msft.csv', parse_dates =True, index_col=0)
 
 df['Adj Close'].plot()
